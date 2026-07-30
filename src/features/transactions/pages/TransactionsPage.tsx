f0b3ecea4d2d19710aac05a84264f4f0227d1a09
Gigilui15 <luigi.naudi@gmail.com>
2026-07-30 22:01:46 +0200

    refactor: move transaction management to transactions page

--- src/features/transactions/pages/TransactionsPage.tsx
+++ src/features/transactions/pages/TransactionsPage.tsx
@@ -1,5 +1,27 @@
+import { AddTransactionForm } from "../components/AddTransactionForm";
+import { BalanceCard } from "../components/BalanceCard";
+import { RecentTransactions } from "../components/RecentTransactions";
+import { useTransactionsContext } from "../TransactionsContext";
+
 const TransactionsPage = () => {
-  return <div>Transaction Page</div>;
+  const { balance, transactions, addTransaction, removeLatestTransaction } =
+    useTransactionsContext();
+
+  return (
+    <div>
+      <BalanceCard balance={balance} />
+      <RecentTransactions transactions={transactions} />
+
+      <button
+        onClick={removeLatestTransaction}
+        disabled={transactions.length === 0}
+      >
+        Remove Latest Transaction
+      </button>
+
+      <AddTransactionForm onAddTransaction={addTransaction} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,8 @@
-import { AddTransactionForm } from "../features/transactions/components/AddTransactionForm";
 import { BalanceCard } from "../features/transactions/components/BalanceCard";
-import { RecentTransactions } from "../features/transactions/components/RecentTransactions";
 import { useTransactionsContext } from "../features/transactions/TransactionsContext";
 
 const DashboardPage = () => {
-  const { transactions, balance, addTransaction, removeLatestTransaction } =
-    useTransactionsContext();
+  const { balance } = useTransactionsContext();
 
   return (
     <section>
@@ -13,16 +10,6 @@ const DashboardPage = () => {
       <p>A simple way to manage your wealth.</p>
 
       <BalanceCard balance={balance} />
-      <RecentTransactions transactions={transactions} />
-
-      <button
-        onClick={removeLatestTransaction}
-        disabled={transactions.length === 0}
-      >
-        Remove Latest Transaction
-      </button>
-
-      <AddTransactionForm onAddTransaction={addTransaction} />
     </section>
   );
 };
